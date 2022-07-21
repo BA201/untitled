ae9aa039d8fc7429550a2fb009918c1f594c8bbd
BA201 <717307787@qq.com>
2022-07-21 14:55:44 +0800

    push git

--- src/main/java/zai/made.java
+++ src/main/java/zai/made.java
@@ -11,5 +11,8 @@ public class made {
         System.out.println("ssh免密登陆");
         System.out.println("ssh免密登陆2.0版本");
         System.out.println("sshmainmi更新了");
+        System.out.println("更新失败");
+
+
     }
 }
