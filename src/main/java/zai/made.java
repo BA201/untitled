5008e8b0fc3077e5c7f5a2e285ec971cd8f1ee95
BA201 <717307787@qq.com>
2022-07-21 13:09:28 +0800

    ok

---
+++ src/main/java/zai/made.java
@@ -0,0 +1,11 @@
+package zai;
+
+public class made {
+    public static void main(String[] args) {
+        System.out.println("我曹666");
+        System.out.println("我曹666");
+        System.out.println("我曹666");
+        System.out.println("我曹666");
+        System.out.println("我曹666");
+    }
+}
